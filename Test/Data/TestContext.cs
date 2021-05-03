b46c2fd468ee0251b0aac1c9deaeb28e3df2d52d
GuidoBekkers <32267783+GuidoBekkers@users.noreply.github.com>
2021-05-03 20:43:33 +0200

    First commit

---
+++ Test/Data/TestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace Test.Data
+{
+    public class TestContext : DbContext
+    {
+        public TestContext (DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
